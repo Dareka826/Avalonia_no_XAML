78ad3972c681d2ad5222a030ba29c3d9fbc691fc
Rin Brk <dareka826@gmail.com>
2023-07-08 16:11:27 +0200

    Add basic application

--- Program.cs
+++ Program.cs
@@ -1,4 +1,34 @@
-﻿class MainClass {
+﻿using Avalonia;
+using Avalonia.Controls;
+
+class MainClass {
     public static void Main(string[] args) {
+        AppBuilder
+            .Configure<Avalonia.Application>()
+            .UsePlatformDetect()
+            .Start(AppMain, args);
+    }
+
+    public static void AppMain(Application app, string[] args) {
+        // Application needs a theme to render window content
+        app.Styles.Add(new Avalonia.Themes.Simple.SimpleTheme());
+        app.RequestedThemeVariant = Avalonia.Styling.ThemeVariant.Default; // Default, Dark, Light
+
+        // Create window
+        var win = new Window();
+        win.Title = "Avalonia no XAML";
+        win.Width = 800;
+        win.Height = 600;
+
+        var text = new Label();
+        win.Content = text;
+
+        text.Content = "Hello from C#!";
+        text.HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center;
+        text.VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center;
+        text.FontSize = 72;
+
+        win.Show();
+        app.Run(win);
     }
 }
